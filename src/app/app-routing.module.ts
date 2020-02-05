225531ee23d16e0c369ad9743c671f5ecf49b60e
Ariel Brenman <abrenman@ocasadomain.com>
2020-02-05 19:05:53 -0300

    ORIGIN

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SelectUsersComponent } from './usuarios/select-users/select-users.component';
+import { TATETIComponent } from './juegos/tateti/tateti.component';
+import { GENERALAComponent } from './juegos/Generala/generala.component';
+import { GENERALAObligadaComponent } from './juegos/GeneralaObligada/generala.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'user-TATETI',component:SelectUsersComponent,data: {juego:"TATETI"}, pathMatch: 'full' },
+  { path: 'TATETI',component:TATETIComponent, pathMatch: 'full' },
+  { path: 'user-GENERALA',component:SelectUsersComponent,data: {juego:"GENERALA"}, pathMatch: 'full' },
+  { path: 'GENERALA',component:GENERALAComponent, pathMatch: 'full' },
+  { path: 'user-GENERALA-Obligada',component:SelectUsersComponent,data: {juego:"GENERALAObligada"}, pathMatch: 'full' },
+  { path: 'GENERALAObligada',component:GENERALAObligadaComponent, pathMatch: 'full' },
+ 
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { JuegosModule } from './juegos/juegos.module';
+
+@NgModule({
+  declarations: [AppComponent,BarraLateralComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    UsuariosModule,
+    JuegosModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
